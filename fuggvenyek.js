bd2ab72712dc4f5dcb568f06f75f30602db4882e
Paróczi Levente Patrik (SZF_2023_2025) <paroczi.levente.patrik@diak.szamalk-szalezi.hu>
2024-03-20 12:56:42 +0100

    Dolgozat

---
+++ fuggvenyek.js
@@ -0,0 +1,12 @@
+import {pingvinlista}from"./index./"
+
+export function konzolraIr(lista) 
+{
+    for (let index = 0; index < lista.length; index++) {
+        
+
+    }
+
+ 
+}
+
---
+++ index.js
@@ -0,0 +1,48 @@
+export const pingvinlista=[
+    {
+    nev:"Kapitány",
+    fajta:"Szamárpingvin",
+    szin:"fekete-fehér",
+    magassag:70-95,
+    szul_datum:2020,
+    szul_hely:"Sydney"    
+    },
+    
+    {
+    nev:"Közlegény",
+    fajta:"kék pingvin",
+    szin:"kék",
+    magassag:30-35,
+    szul_datum:2022,
+    szul_hely:"Belgium"
+    },
+
+    {
+    nev:"István",
+    fajta:"Császárpingvin",
+    szin:"fekete-fehér",
+    magassag:70-80,
+    szul_datum:2021,
+    szul_hely:"Borsod"
+    },
+    {
+    nev:"Szandi",
+    fajta:"Adélie pingivn",
+    szin:"fekete-fehér",
+    magassag:60-70,
+    szul_datum:2019,
+    szul_hely:"Budapest"
+    },
+    {
+    nev:"Atilla",
+    fajta:"pápaszemes pingvin",
+    szin:"feket-fehér",
+    magassag:70-100,
+    szul_datum:2023,
+    szul_hely:"Koppenhága"
+    },
+
+    
+
+
+]
